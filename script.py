c7eb27250e01f22d357360f749c70d977902928b
lorenanicole <me@lorenamesa.com>
2021-03-08 16:20:52 -0600

    Update README

---
+++ script.py
@@ -0,0 +1,22 @@
+import tokenize
+import dis
+import ast
+
+if __name__ == '__main__':
+
+    # Source: https://docs.python.org/3/library/tokenize.html
+    print('Here are the tokens Python identifies when we type `print("Hello PyLadies")`:')
+    with tokenize.open('hello.py') as f:
+        tokens = tokenize.generate_tokens(f.readline)
+        for token in tokens:
+            print(token)
+
+    print('To convert the tokens to bytecode we must first compile it.')
+    tree = ast.parse('print("Hello PyLadies")')
+    compiled_code = compile(tree, 'test.py', mode='exec')
+
+    # Learn more from PEP-0399
+    print('Now here is the byte!')
+    byte_code = dis.dis(compiled_code)
+
+    # execution = interpreter(byte_code)
